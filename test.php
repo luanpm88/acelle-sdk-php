420d7d79cca8502423fd955594c1e78266c039c6
Luan Pham <luanpm88@gmail.com>
2020-11-05 09:47:29 +0700

    test php

--- test.php
+++ test.php
@@ -1,6 +1,10 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-$log = new Monolog\Logger('name');
-$log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
-$log->addWarning('Foo');
+// $log = new Monolog\Logger('name');
+// $log->pushHandler(new Monolog\Handler\StreamHandler('app.log', Monolog\Logger::WARNING));
+// $log->addWarning('Foo sdfs asd asd asda s');
+
+$client = new Acelle\Client('http://acelle.local/api/v1', 'aIpfXqiFMnsvO0e9nlpACZ4x0euvngGTKADSnjKy1760uY5vZ9strUX0f9RW');
+
+$campaigns = $client->campaign()->all();
