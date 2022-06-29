e7dd46dde8a953bec792283842c0f275da2dad2f
Luan Pham <luanpm88@gmail.com>
2022-06-29 22:53:29 +0700

    update

--- src/Resource/MailList.php
+++ src/Resource/MailList.php
@@ -13,4 +13,14 @@ class MailList extends Base {
     public function addCustomField($uid, $params) {
         return $this->makeRequest($uid . '/' . 'add-field', 'POST', $params);
     }
+
+    public function subscribe($uid, $subscriber_uid)
+    {
+        return $this->makeRequest($uid . '/subscribers/' . $subscriber_uid . '/subscribe', 'PATCH');
+    }
+
+    public function unsubscribe($uid, $subscriber_uid)
+    {
+        return $this->makeRequest($uid . '/subscribers/' . $subscriber_uid . '/unsubscribe', 'PATCH');
+    }
 }
